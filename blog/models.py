f9bf9b2ff042115f428d8fb1ee382944a0ebae8e
Alexander Vengrin <alexander.vengrin@gmail.com>
2023-03-17 07:25:03 +0100

    Added security to create/edit.

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    perex = models.TextField(blank=True, null=True) # av 001
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
